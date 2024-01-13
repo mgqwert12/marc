16bb511956bdab29c9a891d70884361df21b49dd
Marc Goes <marcgoes13@gmail.com>
2024-01-13 09:54:10 +0530

    first commit

---
+++ marcgoes208/src/marcgoes208/projectmg.java
@@ -0,0 +1,12 @@
+package marcgoes208;
+
+public class projectmg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ marcgoes208/src/module-info.java
@@ -0,0 +1,2 @@
+module marcgoes208 {
+}
